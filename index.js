3870967d2b5736e8064aa5bf2e92890bcd94e0d6
Raghav Gupta <raghavgupta0399@gmail.com>
2018-10-17 04:02:46 +0530

    Add tokens router to generate random token for the user

--- index.js
+++ index.js
@@ -91,5 +91,6 @@ var unifiedServer = function(req, res) {
 // Router
 var router = {
   'ping' : handlers.ping,
-  'users' : handlers.users
+  'users' : handlers.users,
+  'tokens' : handlers.tokens
 };
